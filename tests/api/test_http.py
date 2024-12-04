c89f1461ba7e28e84268a308fad20ca3e0e81c76
NataKho <khorunzha.natalia96@gmail.com>
2024-12-04 16:05:59 +0100

    add tests, modify pytest.ini

---
+++ tests/api/test_http.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_requests():
+    r = requests.get('http://api.github.com/zen')
+    print(f"Response is {r.text}")
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+    print(headers)
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'github.com'
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 404
